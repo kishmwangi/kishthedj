1a9aae9dd51473c69abc354c1fbec4de97fa87a8
kishmwangi <kishmwangi31@gmail.com>
2025-02-26 01:37:09 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My first React App</h1>
+        <p>My name is Kish and I am a DJ</p>
+        <p>My website is under construction</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/kishthedj_/"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer" 
         >
-          Learn React
+          Follow me on Instagram
         </a>
-      </header>
+      </header> 
+
     </div>
   );
 }
